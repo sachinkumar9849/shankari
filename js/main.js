4fbbf3d2f49a5b45029b52400ef2f1300adf99f9
sachinmadhwa <kjhasachin@gmail.com>
2023-10-17 00:25:04 +0545

    mobile view done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 
+$(document).ready(function () {
+  //jquery for toggle sub menus
+  $(".sub-btn").click(function () {
+    $(this).next(".sub-menu").slideToggle();
+    $(this).find(".dropdown").toggleClass("rotate");
+  });
 
+  //jquery for expand and collapse the sidebar
+  $(".menu-btn").click(function () {
+    $(".side-bar").addClass("active");
+    $(".menu-btn").css("visibility", "hidden");
+  });
+
+  $(".close-btn").click(function () {
+    $(".side-bar").removeClass("active");
+    $(".menu-btn").css("visibility", "visible");
+  });
+});
+// menu mobile end
 $(function() {
   new WOW().init();
 });
